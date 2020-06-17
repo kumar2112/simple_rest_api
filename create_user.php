7570c0cb00cac0f8709727249b51ba1b616bd5ee
kumar2112 <marcussjohnn@gmail.com>
2020-06-17 12:20:57 +0530

    Updated Files

--- create_user.php
+++ create_user.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost/rest-api/");
+header("Access-Control-Allow-Origin: http://localhost/simple-rest-api/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
@@ -17,37 +17,39 @@ $user = new User($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-// set user property values
+if(empty($data->name)){
+    http_response_code(501);
+    echo json_encode(array("message" => "Name is required."));
+    return;
+}
+if(empty($data->email)){
+    http_response_code(502);
+    echo json_encode(array("message" => "Email is required."));
+    return;
+}
+if(empty($data->password)){
+    http_response_code(503);
+    echo json_encode(array("message" => "Password is required."));
+    return;
+}
+if(!$router->checkUnique('email',$data->email)){
+    http_response_code(504);
+    echo json_encode(array("message" => "An user already exists with given email."));
+    return;
+}
+
+//set user property values
 $user->name = $data->name;
 $user->email = $data->email;
 $user->password = $data->password;
-
-if(
-    !empty($user->name) &&
-    !empty($user->email) &&
-    !empty($user->password) &&
-    $user->create()
-){
-
-    // set response code
-    http_response_code(200);
-
-    // display message: user was created
-    echo json_encode(array("message" => "User was created."));
-}
-
-// message if unable to create user
-else{
-
-    // set response code
+if($user->create()){
+  http_response_code(200);
+  // display message: user was created
+  echo json_encode(array("message" => "User was created."));
+}else{
     http_response_code(400);
 
     // display message: unable to create user
     echo json_encode(array("message" => "Unable to create user."));
 }
-
-
-
-
-
- ?>
+?>
--- update_router.php
+++ update_router.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost/rest-api/");
+header("Access-Control-Allow-Origin: http://localhost/simple-rest-api/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
